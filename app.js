9d3c88efc1dd918007225f7a26560d328ea371cb
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2023-07-11 19:58:18 +0530

    return responses

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const http = require('http')
 
 const server =  http.createServer((req,res) => {
     console.log(req)
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My frist page</title><head>')
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    res.write('</html>')
-    res.end();
+   if ('/home' == req.url) {
+     res.end('Welcome home')
+   } 
+   else if('/about' == req.url){
+    res.end('Welcome to About Us page')
+   }
+   else if('/node' == req.url){
+    res.end('Welcome to my Node Js project')
+   }
+   else{
+    res.end('Page is not Found')
+   }
+    
 })
 
-server.listen(3000);
+server.listen(4500);

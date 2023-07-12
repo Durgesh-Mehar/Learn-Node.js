35c9a5701621027f96445334007e692b0108b20d
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2023-07-12 17:44:52 +0530

    clean up the code

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if( url === '/'){
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-    res.write("<body><form action='/message' method ='POST'><input type='text' name='messae'><button>Send</button></from></body>");
-    res.write("</html>");
-    res.end();                                                               
-  }
-  if( url === '/message' && method === 'POST'){
-    fs.writeFileSync("message.txt", "Dummy")
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-});
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);

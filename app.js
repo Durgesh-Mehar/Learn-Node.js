ada7505d6b392b25a0493a6ffccc26a3b576449f
Durgesh-Mehar <120379146+Durgesh-Mehar@users.noreply.github.com>
2023-07-14 17:11:11 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,13 +7,14 @@ const adminRoutes = require('./Routes/admin');
 const shopRoutes = require('./Routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'pubilc')))
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes)
+app.use(adminRoutes);
+app.use(shopRoutes)
 
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>This page Not found</h1>");
+  res.status(404).sendFile(path.join(__dirname,  'views', '404.html'));
 });
 
 
